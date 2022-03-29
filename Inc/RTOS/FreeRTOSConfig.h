aeb2dbff20e0d8c0af2203da89f9da162c33dd5e
padmajamarupaka <padmaja.marupaka@gmail.com>
2022-03-29 20:07:36 +0200

    freertos configuration changed

--- Inc/RTOS/FreeRTOSConfig.h
+++ Inc/RTOS/FreeRTOSConfig.h
@@ -41,9 +41,10 @@
  *----------------------------------------------------------*/
 
 #define configUSE_PREEMPTION		1
+#define configUSE_TIME_SLICING	1
 #define configUSE_IDLE_HOOK			0
 #define configUSE_TICK_HOOK			0
-#define configCPU_CLOCK_HZ			( ( unsigned long ) 72000000 )	
+#define configCPU_CLOCK_HZ			( ( unsigned long ) 48000000 )	
 #define configTICK_RATE_HZ			( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES		( 5 )
 #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 128 )
@@ -52,6 +53,7 @@
 #define configUSE_TRACE_FACILITY	0
 #define configUSE_16_BIT_TICKS		0
 #define configIDLE_SHOULD_YIELD		1
+#define configSUPPORT_STATIC_ALLOCATION	1
 
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES 		0
@@ -63,24 +65,34 @@ to exclude the API function. */
 #define INCLUDE_vTaskPrioritySet		1
 #define INCLUDE_uxTaskPriorityGet		1
 #define INCLUDE_vTaskDelete				1
-#define INCLUDE_vTaskCleanUpResources	0
+#define INCLUDE_vTaskCleanUpResources	1
 #define INCLUDE_vTaskSuspend			1
 #define INCLUDE_vTaskDelayUntil			1
 #define INCLUDE_vTaskDelay				1
+
+/*Software timer related functions.*/
+#define configUSE_TIMERS					1
+#define configTIMER_QUEUE_LENGTH	5
+#define configTIMER_TASK_PRIORITY		3
+#define configTIMER_TASK_STACK_DEPTH 100
 
 /* This is the raw value as per the Cortex-M3 NVIC.  Values can be 255
 (lowest) to 0 (1?) (highest). */
 #define configKERNEL_INTERRUPT_PRIORITY 		255
 /* !!!! configMAX_SYSCALL_INTERRUPT_PRIORITY must not be set to zero !!!!
 See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY 	191 /* equivalent to 0xb0, or priority 11. */
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY 	63/* equivalent to 0xb0, or priority 11. */
+#define configASSERT( x ) if( ( x ) == 0 ) { taskDISABLE_INTERRUPTS(); for( ;; ); }
+#define xPortPendSVHandler PendSV_Handler
+#define xPortSysTickHandler SysTick_Handler
+#define vPortSVCHandler SVC_Handler
 
 
 /* This is the value being used as per the ST library which permits 16
 priority values, 0 to 15.  This must correspond to the
 configKERNEL_INTERRUPT_PRIORITY setting.  Here 15 corresponds to the lowest
 NVIC value of 255. */
-#define configLIBRARY_KERNEL_INTERRUPT_PRIORITY	15
+
 
 #endif /* FREERTOS_CONFIG_H */
 
